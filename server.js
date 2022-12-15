5b3c04fdf5e58b7a84de041d06849edecd607acc
ebonygrrl <abooker77@yahoo.com>
2022-12-15 05:03:54 -0600

    test upload

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+// app requirements
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+// npm package
+const app = express();
+
 // configure port
 const port = process.env.PORT || 3001;
